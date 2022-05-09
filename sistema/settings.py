e5fefdccecf010d6be6564ea43580d7955952247
Kelver Alves Rodrigues <kelver.rodrigues@bonor.local>
2022-05-09 11:33:31 -0300

    modificação no arquivo de settings

--- sistema/settings.py
+++ sistema/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-br2i^kt6es=c+a3^d+&pyoyxd0l(_18grhqy&2w1^dy*g)i)+y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
